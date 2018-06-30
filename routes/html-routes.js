626d3fdfa75542d9473f188a5fa954c21cf08cc4
joedan138 <joedan138@gmail.com>
2018-06-29 22:11:11 -0600

    added header navbar

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,9 +12,17 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // login page
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/form-3/index.html"));
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
+  
+  app.get("/projects", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
+  });
+
+
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
+  });
 };
