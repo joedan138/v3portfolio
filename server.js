07d29666c0a75a9faa2553cab409cca9dbcb6666
joedan138 <joedan138@gmail.com>
2018-06-29 21:34:23 -0600

    wokring with node

---
+++ server.js
@@ -0,0 +1,36 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 7000;
+
+
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static(__dirname + "/public"));
+
+
+
+// Routes
+// =============================================================
+
+require("./routes/html-routes.js")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);  });
